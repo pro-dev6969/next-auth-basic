f20945b3c75f45dfd4b522eac3c635b7dce35fb3
Vedant <vedantbhardwaj10@gmail.com>
2024-08-20 16:08:15 +0530

    git provider changes

--- auth.ts
+++ auth.ts
@@ -27,8 +27,8 @@ export const { handlers:{ GET , POST} , auth , signIn , signOut } = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
           }),
           GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientId: process.env.MY_GITHUB_ID,
+            clientSecret: process.env.MY_GITHUB_SECRET
           })
       ],
       secret: process.env.NEXTAUTH_SECRET
