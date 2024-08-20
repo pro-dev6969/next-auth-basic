411de74c53d755d7a1702fb10d9fece0e767412f
Vedant <vedantbhardwaj10@gmail.com>
2024-08-20 16:00:34 +0530

    init

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,3 @@
+// NOTE : Next Auth is a session management library  , that's why it doesn't provide any signup page 
+
+export {GET , POST} from "@/auth"
---
+++ auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+export const { handlers:{ GET , POST} , auth , signIn , signOut } = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+              username: { label: 'email', type: 'text', placeholder: '' },
+              password: { label: 'password', type: 'password', placeholder: '' },
+            },
+            async authorize(credentials: any) {
+                // whatever u fill in the form ,can be accessed using credentials
+                const email = credentials.email;
+                const password = credentials.password;
+                
+                // return what you want to be stored in the jwt token
+                return {
+                    id: "user1"
+                };
+            },
+          }),
+          GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+          }),
+          GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+          })
+      ],
+      secret: process.env.NEXTAUTH_SECRET
+})
